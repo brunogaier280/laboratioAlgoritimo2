1f17d8ef93e3042bdb3b8633fb58ebc3e0f1ae15
brunogaier280 <126731573+brunogaier280@users.noreply.github.com>
2023-09-03 15:49:48 -0300

    Create Parte1-exercicio04.py

---
+++ Exercício aula4/Parte1-exercicio04.py
@@ -0,0 +1,9 @@
+
+my_dicionario = {"chave1": 10, "chave2": 20, "chave3": 30}
+
+chave_para_verificar = "chave2"
+
+if chave_para_verificar in my_dicionario:
+    print("Verdadeiro")
+else:
+    print("Falso")
