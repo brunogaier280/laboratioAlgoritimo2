e27fa2415550c839dc680f791123b864b4bbe54f
brunogaier280 <126731573+brunogaier280@users.noreply.github.com>
2023-11-07 22:16:31 -0300

    Create Exercicio04.py

---
+++ aula do dia 7 do 11/Exercicio04.py
@@ -0,0 +1,16 @@
+def find_element(lista, indice):
+    try:
+        elemento = lista[indice]
+        return elemento
+    except IndexError:
+        raise IndexError("Índice inválido. O índice está fora dos limites da lista.")
+
+# Exemplo de uso da função
+minha_lista = [1, 2, 3, 4, 5]
+indice = 7
+
+try:
+    resultado = find_element(minha_lista, indice)
+    print("Elemento encontrado:", resultado)
+except IndexError as e:
+    print(e)
